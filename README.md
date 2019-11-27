18863c0aff944c7c56e3ff56460ba595293727af
rongfengliang <1141591465@qq.com>
2019-11-27 15:06:11 +0800

    change docs

---
+++ README.md
@@ -0,0 +1,9 @@
+# some benchmark for web 
+
+## how to running
+
+* [json-parse](./json-parse/app.js)
+
+```code
+yarn app:json-parse
+```
---
+++ json-parse/app.js
@@ -0,0 +1,28 @@
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite;
+const userinfo = {
+    name: "dalong", age: 333, items: [
+        {
+            name: "dalong",
+            age: 333
+        },
+        {
+            name: "dalong22",
+            age: 33
+        }
+    ]
+}
+const userinfo_str = JSON.stringify(userinfo)
+suite.add("ob", () => {
+    JSON.parse(userinfo)
+}).add("str", () => {
+    JSON.parse(userinfo_str)
+})
+    .on('cycle', function (event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    .run({ 'async': false });
+
